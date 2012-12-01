25d829f57c4899eb5b309428760ee4460114d383
Nathan Zadoks <nathan@nathan7.eu>
2012-12-01 14:42:08 +0100

    daemonize using child_process removes the daemon dependency (native modules are bad)

--- dotjsd.js
+++ dotjsd.js
@@ -16,8 +16,17 @@ if (argv.d === true) argv.d = path.join('/tmp', process.env.USER + '_djsd.pid');
 process.chdir(argv.h);
 
 if (argv.d) {
-    var daemon = require('daemon');
-    daemon.daemonize(argv.l, argv.d);
+    var child_process = require('child_process');
+    var fs = require('fs');
+    var args = [process.argv[1]];
+    Object.keys(argv).forEach(function(key){
+        if (key.match(/^[a-z]$/) && ['d', 'l'].indexOf(key) != -1) args.push('-' + key, argv[key]);
+    });
+    child_process.spawn(process.argv[0], args, {
+        detached: true,
+        stdio: ['ignore', fs.openSync(argv.l, 'a'), fs.openSync(argv.l, 'a')]
+    });
+    process.exit();
 }
 
 var debug = require('debug')('http');
